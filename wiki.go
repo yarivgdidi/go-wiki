7fa2727972619201df1af651be20c044112f7a81
Yariv <yariv.gdidi@gmail.com>
2021-09-21 23:48:25 +0300

    first commit

--- wiki.go
+++ wiki.go
@@ -39,10 +39,6 @@ func renderTemplate(w http.ResponseWriter, templateName string, p *Page) {
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
-	// title, err := getTitle(w, r)
-	// if err != nil {
-	// 	return
-	// }
 	p, err := loadPage(title)
 	if err != nil {
         http.Redirect(w, r, "/edit/"+title, http.StatusFound)
@@ -52,10 +48,6 @@ func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request, title string) {
-	// title, err := getTitle(w, r)
-	// if err != nil {
-	// 	return
-	// }
 	p, err := loadPage(title)
 	if err != nil {
 		p = &Page{Title: title}
@@ -63,10 +55,6 @@ func editHandler(w http.ResponseWriter, r *http.Request, title string) {
 	renderTemplate(w, "edit", p)
 }
 func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
-	// title, err := getTitle(w, r)
-	// if err != nil {
-	// 	return
-	// }
 	body := r.FormValue("body")
 	p := &Page{Title: title, Body: []byte(body)}
 	err := p.save()
@@ -76,15 +64,6 @@ func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
 	}
 	http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
-
-// func getTitle( w http.ResponseWriter, r *http.Request) (string, error) {
-// 	m := validPath.FindStringSubmatch(r.URL.Path)
-// 	if m == nil {
-// 		http.NotFound(w, r)
-// 		return "", errors.New("invalid Page Title")
-// 	}
-// 	return m[2], nil
-// }
 
 func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
